f0dc07e23953aa8614decc626d19506664f05849
screeeen <miguel@screenimplosion.com>
2020-11-04 00:00:03 +0100

    some legends

--- src/components/Container.js
+++ src/components/Container.js
@@ -12,6 +12,8 @@ const Container = ({ searchTerm }) => {
 
   return (
     <div className="photo-container">
+      <h3>Click on the pics to see the markers</h3>
+      <h3>Click on the markers to see the pics</h3>
       {loading ? <Loader /> : <Gallery data={images} />}
     </div>
   );
